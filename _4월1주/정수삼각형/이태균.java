f3e5bbc51e13b3f6b2fc5a48cc8e29055537da57
taegyun1995 <needle12s@gmail.com>
2025-04-05 10:03:11 +0900

    lv3: 정수삼각형

--- _4월1주/정수삼각형/이태균.java
+++ _4월1주/정수삼각형/이태균.java
@@ -9,6 +9,7 @@ public class 이태균 {
     }
 
     public static int solution(int[][] triangle) {
+        // DP 알고리즘 상향식(bottom-up) 접근법
         // 뒤에서부터 계산하면서 삼각형 배열을 직접 업데이트
         for (int row = triangle.length - 2; row >= 0; row--) {
             for (int col = 0; col < triangle[row].length; col++) {
@@ -17,7 +18,7 @@ public class 이태균 {
             }
         }
 
-        // 최종적으로 triangle[0][0]에 최적 경로 합이 들어 있습니다.
+        // 전체 삼각형에서 가능한 최대 경로의 합이 저장
         return triangle[0][0];
     }
 }
